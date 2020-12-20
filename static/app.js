c9b0f6bd6d61a00c3520e9403ea33ee41db76da6
Dino Angelov <dino@dangelov.com>
2020-12-20 13:06:14 -0600

    display additional information with search results

--- static/app.js
+++ static/app.js
@@ -5,18 +5,38 @@ const Controller = {
     const data = Object.fromEntries(new FormData(form));
     const response = fetch(`/search?q=${data.query}`).then((response) => {
       response.json().then((results) => {
-        Controller.updateTable(results);
+        Controller.updateTable(results.results);
+        Controller.showTimeTaken(results.time, results.results.length);
+        Controller.showSearchCorrection(results.replaced);
       });
     });
   },
 
   updateTable: (results) => {
-    const table = document.getElementById("table-body");
+    const table = document.getElementById("results");
     const rows = [];
     for (let result of results) {
-      rows.push(`<tr>${result}<tr/>`);
+      rows.push(`<li>${result}</li>`);
     }
-    table.innerHTML = rows;
+    table.innerHTML = rows.join("");
+  },
+
+  showTimeTaken: (timeTaken, numResults) => {
+    const span = document.getElementById("time-taken");
+    span.innerHTML = "Found " + numResults + " results in "+timeTaken;
+  },
+
+  showSearchCorrection: (replaced) => {
+    const span = document.getElementById("search-correction");
+    var query = document.getElementById("query").value
+    if (replaced.length > 0) {
+      for (i = 0; i < replaced.length; i+=2) {
+        query = query.replaceAll(replaced[i], "<em>"+replaced[i+1]+"</em>")
+      }
+      span.innerHTML = "Searching for "+query;
+    } else {
+      span.innerHTML = "";
+    }
   },
 };
 
